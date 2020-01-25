2f2268db9826e08ec372dedb9c20ca82af50dc05
xylimeng <xylimeng@gmail.com>
2020-01-25 13:27:08 -0600

    clean folder

--- BayesianFQR/invChol.m
+++
@@ -1,7 +0,0 @@
-function A_inv=invChol(A)
-
-R=chol(A);
-
-R_inv=inv(R);
-
-A_inv=R_inv*R_inv';
--- BayesianQR/check_function.m
+++
@@ -1,9 +0,0 @@
-function pho=check_function(x,q)
-% This function calculates the check function pho(x) at quantile q.
-
-[m,n]=size(x);
-pho=zeros(m,n);
-pho(x>0)=q*x(x>0);
-pho(x<0)=(q-1)*x(x<0);
-
-
